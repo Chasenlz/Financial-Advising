1fc6985038471be5b7699f0ccefd0f37bb8ec38c
Chase Lazarian <40992932+Chasenlz@users.noreply.github.com>
2023-06-20 18:55:50 -0500

    Update front.js

--- front.js
+++ front.js
@@ -1,7 +1,39 @@
 function openForm() {
     document.getElementById("popup-form").style.display = "block";
 }
-  
+
 function closeForm() {
     document.getElementById("popup-form").style.display = "none";
+}
+
+function showRegisterForm() {
+    document.getElementById("popup").setAttribute("onsubmit", "register(event)");
+    document.querySelector(".btn").innerHTML = "Register";
+    document.querySelector(".login-link").setAttribute("onclick", "showLoginForm()");
+    document.querySelector(".login-link").innerHTML = "Login";
+}
+
+function showLoginForm() {
+    document.getElementById("popup").setAttribute("onsubmit", "login(event)");
+    document.querySelector(".btn").innerHTML = "Login";
+    document.querySelector(".login-link").setAttribute("onclick", "showRegisterForm()");
+    document.querySelector(".login-link").innerHTML = "Register";
+}
+
+function register(event) {
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    // Perform registration logic here
+    console.log("Register:", username);
+    // Redirect to the home page or perform any other necessary actions
+    window.location.href = "index.html";
+}
+
+function login(event) {
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    // Perform login logic here
+    console.log("Login:", username);
+    // Redirect to the home page or perform any other necessary actions
+    window.location.href = "index.html";
 }
